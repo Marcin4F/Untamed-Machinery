17fc346f4c201170a1bc151e6a3f902d6c8582e7
Marcin4F <markost4f@gmail.com>
2026-05-27 23:30:44 +0200

    nowe menu glowne

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,22 +5,54 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    [SerializeField] Button play, quit;
+    [Header("Ekrany Menu (Panels)")]
+    [SerializeField] GameObject noSavePanel;    // NIE ma zapisu gry
+    [SerializeField] GameObject hasSavePanel;   // jest zapis gry
+
+    [Header("Przyciski - Ekran BEZ Zapisu")]
+    [SerializeField] Button startGameButton;
+    [SerializeField] Button quitNoButton;
+
+    [Header("Przyciski - Ekran Z Zapisem")]
+    [SerializeField] Button newGameButton;
+    [SerializeField] Button continueButton;
+    [SerializeField] Button quitHasButton;
 
     private void Start()
     {
-        play.onClick.AddListener(StartGame);
-        quit.onClick.AddListener(CloseGame);
+        startGameButton.onClick.AddListener(StartGame);
+        newGameButton.onClick.AddListener(StartGame);
+        continueButton.onClick.AddListener(ContinueGame);
+        quitNoButton.onClick.AddListener(CloseGame);
+        quitHasButton.onClick.AddListener(CloseGame);
+        
+        bool hasSave = SaveSystem.HasSave();
+        if (hasSave)
+        {
+            hasSavePanel.SetActive(true);
+            noSavePanel.SetActive(false);
+        }
+        else
+        {
+            hasSavePanel.SetActive(false);
+            noSavePanel.SetActive(true);
+        }
     }
 
     void StartGame()
     {
+        SaveSystem.loadFromSave = false;
+        SceneManager.LoadScene(1);
+    }
+
+    void ContinueGame()
+    {
+        SaveSystem.loadFromSave = true;
         SceneManager.LoadScene(1);
     }
 
     void CloseGame()
     {
-        Debug.Log("Wyjscie");
         Application.Quit();
     }
 }
